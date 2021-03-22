d3e7dc473d146394d0dccdcd80ebfdc02e327eb9
Maksim <max.freedman.main@gmail.com>
2021-03-22 18:06:08 +0200

    added description to UserNotFoundException

--- src/main/java/exception/userscore/UserNotFoundException.java
+++ src/main/java/exception/userscore/UserNotFoundException.java
@@ -1,5 +1,10 @@
 package exception.userscore;
 
+/**
+ * Пользовательское исключение, генерируется,
+ * если пользователь не найден в БД или другом хранилище (массиве или списке).
+ * */
+
 public class UserNotFoundException extends RuntimeException {
 
     public UserNotFoundException(String message) {
