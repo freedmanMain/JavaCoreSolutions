f04c529e57ef9a8ea834e11feccd9c4d4f741848
Maksim <max.freedman.main@gmail.com>
2021-03-22 18:59:16 +0200

    solved Password Validation task!

---
+++ src/test/java/exception/userscore/passwordvalidation/UserServiceTest.java
@@ -0,0 +1,28 @@
+package exception.userscore.passwordvalidation;
+
+import exception.userscore.passwordvalidation.validationexception.PasswordValidationException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    User user1 = new User(
+            "test@gmail.com",
+            "12345678910",
+            "12345678910");
+
+    User user2 = new User(
+            "test@gmail.com",
+            "1234567891011",
+            "12345678910");
+
+    UserService service = new UserService();
+    PasswordValidator validator = new PasswordValidator();
+
+    @Test
+    public void test_user_service_valid_password() {
+        assertTrue(validator.validate(user1.getPassword(), user1.getRepeatPassword()));
+        assertFalse(validator.validate(user2.getPassword(), user2.getRepeatPassword()));
+    }
+}
